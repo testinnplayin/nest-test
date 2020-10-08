08099bb0b551ab39bdffc388811dd3e4637778c1
testinnplayin <archaurore@gmail.com>
2020-10-08 10:48:24 +0200

    added some tests

--- src/things/thing.middleware.ts
+++ src/things/thing.middleware.ts
@@ -3,9 +3,8 @@ import { Request, Response } from 'express';
 
 @Injectable()
 export class ThingMiddleware implements NestMiddleware {
-    // eslint-disable-next-line @typescript-eslint/ban-types
-    use(req: Request, res: Response, next: Function) {
-        console.log(`This is the request params: ${req.params}`);
+    use(req: Request, res: Response, next: any) {
+        console.log(`This is the request params: ${JSON.stringify(req.params)}`);
         next();
     }
 }
--- src/things/things.controller.spec.ts
+++ src/things/things.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Thing } from './thing.interface';
 import { ThingsController } from './things.controller';
+import { ThingsService } from './things.service';
 
 describe('ThingsController', () => {
   let controller: ThingsController;
@@ -7,6 +9,7 @@ describe('ThingsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ThingsController],
+      providers: [ThingsService],
     }).compile();
 
     controller = module.get<ThingsController>(ThingsController);
@@ -14,5 +17,21 @@ describe('ThingsController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('root', () => {
+    it('should return a list of things', async () => {
+      const data = await controller.getThings();
+      expect(data).toHaveLength(3);
+    });
+
+    it('should return a single thing', async () => {
+      const data = await controller.getThing('1');
+      expect(data).toMatchObject({
+        name: 'Thing2',
+        key1: 'baz',
+        key2: 'boz',
+      });
+    });
   });
 });
